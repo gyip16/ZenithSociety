977fb0b9a1002dfa9c9c66bcda79f4a53950f90a
gyip16 <gyip16@my.bcit.ca>
2017-10-14 23:19:34 -0700

    migrationfix

---
+++ ZenithDataLib/ActivityCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZenithDataLib;
+
+namespace ZenithDataLib
+
+{
+    public class ActivityCategory
+    {
+        [Key]
+        public int ActivityCategoryId { get; set; }
+        public string ActivityDescription { get; set; }
+        public DateTime CreationDate { get; set; }
+
+        public List<Event> Events { get; set; }
+    }
+}
---
+++ ZenithDataLib/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZenithDataLib
+{
+    public class Event
+    {
+        [Key]
+        public int EventId { get; set; }
+        public DateTime EventFromDateAndTime { get; set; }
+        public DateTime EventToDateAndTime { get; set; }
+        public string EnteredByUsername { get; set; }
+
+        public int ActivityCategoryId { get; set; }
+        public ActivityCategory ActivityCategory { get; set; }
+
+        public DateTime CreationDate { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
